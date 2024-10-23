9d0d6ebb012ba416bb5029870e0cda65663c02c0
Thomas Sundberg <thomas@thinkcode.se>
2024-10-23 17:12:10 +0200

    The end state when applying The Four Simple Rules of desig at JDD 2024

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -1,11 +1,8 @@
 
-public class TennisGame2 implements TennisGame
-{
+public class TennisGame2 implements TennisGame {
     public int P1point = 0;
     public int P2point = 0;
-    
-    public String P1res = "";
-    public String P2res = "";
+
     private String player1Name;
     private String player2Name;
 
@@ -14,122 +11,78 @@ public class TennisGame2 implements TennisGame
         this.player2Name = player2Name;
     }
 
-    public String getScore(){
+    @Override
+    public void wonPoint(String player) {
+        if (player.equals("player1")) {
+            P1point++;
+        } else {
+            P2point++;
+        }
+    }
+
+    @Override
+    public String getScore() {
         String score = "";
-        if (P1point == P2point && P1point < 4)
-        {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
+
+        score = equal(score);
+        score = deuce(score);
+        score = playerWinPoint(score);
+        score = advantage(score);
+        score = winner(score);
+
+        return score;
+    }
+
+    private String equal(String score) {
+        // equals
+        if (P1point == P2point && P1point < 4) {
+            score = getPointName(P1point);
             score += "-All";
         }
-        if (P1point==P2point && P1point>=3)
+        return score;
+    }
+
+    private String deuce(String score) {
+        if (P1point == P2point && P1point >= 3)
             score = "Deuce";
-        
-        if (P1point > 0 && P2point==0)
-        {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
-            P2res = "Love";
+        return score;
+    }
+
+    private String playerWinPoint(String score) {
+        if (P1point != P2point && P2point < 4 && P1point < 4) {
+            String P1res = getPointName(P1point);
+            String P2res = getPointName(P2point);
+
             score = P1res + "-" + P2res;
         }
-        if (P2point > 0 && P1point==0)
-        {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        
-        if (P1point>P2point && P1point < 4)
-        {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point>P1point && P2point < 4)
-        {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
-        }
-        
-        if (P1point > P2point && P2point >= 3)
-        {
+
+        return score;
+    }
+
+    private String advantage(String score) {
+        if (P1point > P2point && P2point >= 3) {
             score = "Advantage player1";
         }
-        
-        if (P2point > P1point && P1point >= 3)
-        {
+
+        if (P2point > P1point && P1point >= 3) {
             score = "Advantage player2";
         }
-        
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-        {
+        return score;
+    }
+
+    private String winner(String score) {
+        if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
             score = "Win for player1";
         }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-        {
+        if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
             score = "Win for player2";
         }
         return score;
     }
-    
-    public void SetP1Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
-            P1Score();
-        }
-            
-    }
-    
-    public void SetP2Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
-            P2Score();
-        }
-            
-    }
-    
-    public void P1Score(){
-        P1point++;
-    }
-    
-    public void P2Score(){
-        P2point++;
-    }
 
-    public void wonPoint(String player) {
-        if (player == "player1")
-            P1Score();
-        else
-            P2Score();
+    private String getPointName(int point) {
+        String[] names = {"Love", "Fifteen", "Thirty", "Forty"};
+
+        return names[point];
     }
 }
